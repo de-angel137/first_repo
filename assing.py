48cc3ab9b0f087ab4c1148d2bb9f18ad4c32da88
de-angel137 <vgreat086@gmail.com>
2026-02-11 14:32:47 +0100

    Add files via upload

---
+++ assing.py
@@ -0,0 +1,12 @@
+print("=== Rectangular Measurement ===")
+
+length = float(input("Enter the width: "))
+width = float(input("Enter the height: "))
+
+area = length * width
+
+perimeter = 2 * length + width
+
+print("\n --- Result ---")
+print("Area:", area)
+print("Perimeter:", perimeter)
