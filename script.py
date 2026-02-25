5aad9ed9378689320798f7ea2ead3e4e921e6b5d
nullx <vgreat086@gmail.com>
2026-02-25 10:36:18 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,15 @@
+print("=== Variables ===")
+num1 = 25
+num2 = 4
+
+print("First Number:",num1)
+print("Second Number:",num2)
+
+print("\n === Result ===")
+print("Sum:",num1 + num2)
+print("Difference:",num1 - num2)
+print("Product:",num1 * num2)
+print("Division:",num1 / num2)
+print("Floor Division :",num1 // num2)
+print("Modulo:",num1 % num2)
+print("Power:",num1 ** num2)
